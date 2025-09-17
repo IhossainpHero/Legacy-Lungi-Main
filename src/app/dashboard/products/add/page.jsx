c4079ac5068c598fb501a73b5817947a74095eac
IhossainpHero <imranmollah9131@gmai;.com>
2025-09-17 11:08:06 +0600

    Next public add

--- src/app/dashboard/products/add/page.jsx
+++ src/app/dashboard/products/add/page.jsx
@@ -38,11 +38,14 @@ export default function AddProductPage() {
 
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("upload_preset", process.env.CLOUDINARY_UPLOAD_PRESET);
+    formData.append(
+      "upload_preset",
+      process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET
+    );
 
     try {
       const res = await fetch(
-        `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`,
+        `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
         {
           method: "POST",
           body: formData,
